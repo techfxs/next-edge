94149f9e3e8bdbe566309891ba1d73aae9f5c33b
TechFxs <techfxs@gmail.com>
2025-11-10 13:41:58 -0800

    feat: setup redirections

--- middleware.ts
+++ middleware.ts
@@ -8,27 +8,49 @@ export function middleware(request: NextRequest) {
     const referer = request.headers.get("referer") || "";
     const refererUrl = referer ? new URL(referer) : null;
     const isProductPage = refererUrl?.pathname.startsWith("/product");
+    const isCheckoutPage = refererUrl?.pathname.startsWith("/checkout");
+    const isCartPage = refererUrl?.pathname.startsWith("/cart");
 
-    const origin = isProductPage
-      ? "https://nx-proto-mw-pdp.vercel.app"
-      : "https://nx-proto-mw-home.vercel.app";
+    let origin = "https://mw-mono-storefront.vercel.app/";
+    if (isProductPage) {
+      origin = "https://mw-mono-product.vercel.app/";
+    } else if (isCheckoutPage) {
+      origin = "https://mw-mono-checkout.vercel.app/"; // Update with your checkout origin
+    } else if (isCartPage) {
+      origin = "https://mw-mono-cart.vercel.app/"; // Update with your cart origin
+    }
 
     const targetUrl = new URL(url.pathname, origin);
     targetUrl.search = url.search; // Preserve query parameters
     return NextResponse.rewrite(targetUrl);
   }
 
+  if (url.pathname.startsWith("/checkout")) {
+    return NextResponse.rewrite("https://mw-mono-checkout.vercel.app/"); // Update with your checkout origin
+  }
+
+  if (url.pathname.startsWith("/cart")) {
+    return NextResponse.rewrite("https://mw-mono-cart.vercel.app/"); // Update with your cart origin
+  }
+
   if (url.pathname.startsWith("/product")) {
-    return NextResponse.rewrite("https://nx-proto-mw-pdp.vercel.app/");
+    return NextResponse.rewrite("https://mw-mono-product.vercel.app/");
   }
 
   if (url.pathname.startsWith("/")) {
-    return NextResponse.rewrite("https://nx-proto-mw-home.vercel.app/");
+    return NextResponse.rewrite("https://mw-mono-storefront.vercel.app/");
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/product", "/_next/:path*", "/old-path", "/api/:path*"],
+  matcher: [
+    "/",
+    "/product/:path*",
+    "/checkout/:path*",
+    "/cart/:path*",
+    "/_next/:path*",
+    "/api/:path*",
+  ],
 };
